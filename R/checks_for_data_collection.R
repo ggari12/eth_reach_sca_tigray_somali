9545f1580a36f3b11a6d40e22cd68fab7104834b
ACTED\Getu Gari <getu.boja22@yahoo.com>
2024-03-19 09:34:51 +0300

    update min survey times

--- R/checks_for_data_collection.R
+++ R/checks_for_data_collection.R
@@ -6,6 +6,8 @@ library(lubridate)
 library(glue)
 library(supporteR)
 
+source("R/composite_indicators.R")
+
 # read data and tool ----------------------------------------------------------
 # data
 data_path <- "inputs/ETH2306a_SCA_Tigray_Somali_data.xlsx"
@@ -17,7 +19,14 @@ df_tool_data <- readxl::read_excel(data_path) |>
          `_geopoint_longitude` = as.numeric(`_gps_longitude`),
          enumerator_id = ifelse(is.na(enumerator_id), enum_id, enumerator_id)) |> 
   checks_add_extra_cols(input_enumerator_id_col = "enumerator_id",
-                        input_location_col = "woreda") 
+                        input_location_col = "woreda") |> 
+  create_composite_indicators() |> 
+  rowwise() |> 
+  mutate(int.total_enrolled_boys = sum(c_across(c("pre_school_enrolled_boys", "primary_school_enrolled_boys", "post_primary_school_enrolled_boys", "middle_school_enrolled_boys", "high_school_enrolled_boys", "preparatory_school_enrolled_boys")), na.rm = T),
+         int.total_enrolled_girls = sum(c_across(c("pre_school_enrolled_girls", "primary_school_enrolled_girls", "post_primary_school_enrolled_girls", "middle_school_enrolled_girls", "high_school_enrolled_girls", "preparatory_school_enrolled_girls")), na.rm = T),        
+         #int.total_enrolled_children = sum(c_across(c("pre_school_enrolled_total", "primary_school_enrolled_total", "post_primary_school_enrolled_total", "middle_school_enrolled_total", "high_school_enrolled_total", "preparatory_school_enrolled_total")), na.rm = T)
+  )|>
+  ungroup()
 
 # tool
 loc_tool <- "inputs/ETH2306a_SCA_Tigray_Somali_tool.xlsx"
@@ -56,7 +65,7 @@ add_checks_data_to_list(input_list_name = "checks_output", input_df_name = "df_t
 # Time checks -------------------------------------------------------------
 
 # Time interval for the survey
-min_time_of_survey <- 15
+min_time_of_survey <- 7
 max_time_of_survey <- 120
 
 df_c_survey_time <-  supporteR::check_survey_time(input_tool_data = df_tool_data, 
@@ -134,7 +143,131 @@ df_greater_thresh_distance <- check_threshold_distance(input_sample_data = df_sa
 add_checks_data_to_list(input_list_name = "checks", input_df_name = "df_greater_thresh_distance")
 
 # logical checks --------------------------------------------------------------
+# number of boys with disabilities should be not greater than total enrolled boys
+df_logic_c_boys_with_disabilities_not_grthan_enrolled_boys <- df_tool_data |> 
+  filter(boys_with_disabilities > int.total_enrolled_boys) |> 
+  mutate(i.check.type = "change_response",
+         i.check.name = "boys_with_disabilities",
+         i.check.current_value = as.character(boys_with_disabilities),
+         i.check.value = "NA",
+         i.check.issue_id = "logic_c_boys_with_disabilities_not_grthan_enrolled_boys",
+         i.check.issue = glue("boys_with_disabilities_not_grthan_enrolled_boys, boys_with_disabilities: {boys_with_disabilities} but int.total_enrolled_boys: {int.total_enrolled_boys}"),
+         i.check.other_text = "",
+         i.check.checked_by = "",
+         i.check.checked_date = as_date(today()),
+         i.check.comment = "", 
+         i.check.reviewed = "",
+         i.check.adjust_log = "",
+         i.check.so_sm_choices = "") |> 
+  filter(!is.na(i.check.current_value)) |> 
+  supporteR::batch_select_rename(input_selection_str = "i.check.", input_replacement_str = "")
 
+add_checks_data_to_list(input_list_name = "checks_output", input_df_name = "df_logic_c_boys_with_disabilities_not_grthan_enrolled_boys")
+
+# number of girls with disabilities should be not greater than total enrolled girls
+df_logic_c_girls_with_disabilities_not_grthan_enrolled_girls <- df_tool_data |> 
+  filter(girls_with_disabilities > int.total_enrolled_girls) |> 
+  mutate(i.check.type = "change_response",
+         i.check.name = "girls_with_disabilities",
+         i.check.current_value = as.character(girls_with_disabilities),
+         i.check.value = "NA",
+         i.check.issue_id = "logic_c_girls_with_disabilities_not_grthan_enrolled_girls",
+         i.check.issue = glue("girls_with_disabilities_not_grthan_enrolled_girls, girls_with_disabilities: {girls_with_disabilities} but int.total_enrolled_girls: {int.total_enrolled_girls}"),
+         i.check.other_text = "",
+         i.check.checked_by = "",
+         i.check.checked_date = as_date(today()),
+         i.check.comment = "", 
+         i.check.reviewed = "",
+         i.check.adjust_log = "",
+         i.check.so_sm_choices = "") |> 
+  filter(!is.na(i.check.current_value)) |> 
+  supporteR::batch_select_rename(input_selection_str = "i.check.", input_replacement_str = "")
+
+add_checks_data_to_list(input_list_name = "checks_output", input_df_name = "df_logic_c_girls_with_disabilities_not_grthan_enrolled_girls")
+
+# number of boys dropouts should be not greater than total enrolled boys
+df_logic_c_boys_dropped_out_not_grthan_enrolled_boys <- df_tool_data |> 
+  filter(boys_dropped_out > int.total_enrolled_boys) |> 
+  mutate(i.check.type = "change_response",
+         i.check.name = "boys_dropped_out",
+         i.check.current_value = as.character(boys_dropped_out),
+         i.check.value = "NA",
+         i.check.issue_id = "logic_c_boys_dropped_out_not_grthan_enrolled_boys",
+         i.check.issue = glue("boys_dropped_out_not_grthan_enrolled_boys, boys_dropped_out: {boys_dropped_out} but int.total_enrolled_boys: {int.total_enrolled_boys}"),
+         i.check.other_text = "",
+         i.check.checked_by = "",
+         i.check.checked_date = as_date(today()),
+         i.check.comment = "", 
+         i.check.reviewed = "",
+         i.check.adjust_log = "",
+         i.check.so_sm_choices = "") |> 
+  filter(!is.na(i.check.current_value)) |> 
+  supporteR::batch_select_rename(input_selection_str = "i.check.", input_replacement_str = "")
+
+add_checks_data_to_list(input_list_name = "checks_output", input_df_name = "df_logic_c_boys_dropped_out_not_grthan_enrolled_boys")
+
+# number of girls dropouts should be not greater than total enrolled girls
+df_logic_c_girls_dropped_out_not_grthan_enrolled_girls <- df_tool_data |> 
+  filter(girls_dropped_out > int.total_enrolled_girls) |> 
+  mutate(i.check.type = "change_response",
+         i.check.name = "girls_dropped_out",
+         i.check.current_value = as.character(girls_dropped_out),
+         i.check.value = "NA",
+         i.check.issue_id = "logic_c_girls_dropped_out_not_grthan_enrolled_girls",
+         i.check.issue = glue("girls_dropped_out_not_grthan_enrolled_girls, girls_dropped_out: {girls_dropped_out} but int.total_enrolled_girls: {int.total_enrolled_girls}"),
+         i.check.other_text = "",
+         i.check.checked_by = "",
+         i.check.checked_date = as_date(today()),
+         i.check.comment = "", 
+         i.check.reviewed = "",
+         i.check.adjust_log = "",
+         i.check.so_sm_choices = "") |> 
+  filter(!is.na(i.check.current_value)) |> 
+  supporteR::batch_select_rename(input_selection_str = "i.check.", input_replacement_str = "")
+
+add_checks_data_to_list(input_list_name = "checks_output", input_df_name = "df_logic_c_girls_dropped_out_not_grthan_enrolled_girls")
+
+# number of idp enrolled children should be not greater than total enrolled children
+df_logic_c_idp_enrolled_children_not_grthan_total_enrolled_children <- df_tool_data |> 
+  filter(idp_enrolled_children > int.total_enrolled_children) |> 
+  mutate(i.check.type = "change_response",
+         i.check.name = "idp_enrolled_children",
+         i.check.current_value = as.character(idp_enrolled_children),
+         i.check.value = "NA",
+         i.check.issue_id = "logic_c_idp_enrolled_children_not_grthan_enrolled_girls",
+         i.check.issue = glue("idp_enrolled_children_not_grthan_total_enrolled_children, idp_enrolled_children: {idp_enrolled_children} but int.total_enrolled_children: {int.total_enrolled_children}"),
+         i.check.other_text = "",
+         i.check.checked_by = "",
+         i.check.checked_date = as_date(today()),
+         i.check.comment = "", 
+         i.check.reviewed = "",
+         i.check.adjust_log = "",
+         i.check.so_sm_choices = "") |> 
+  filter(!is.na(i.check.current_value)) |> 
+  supporteR::batch_select_rename(input_selection_str = "i.check.", input_replacement_str = "")
+
+add_checks_data_to_list(input_list_name = "checks_output", input_df_name = "df_logic_c_idp_enrolled_children_not_grthan_total_enrolled_children")
+
+# number of students attending shool now should be not greater than total enrolled children
+df_logic_c_attending_shool_now_not_grthan_total_enrolled_children <- df_tool_data |> 
+  filter(attending_shool_now > int.total_enrolled_children) |> 
+  mutate(i.check.type = "change_response",
+         i.check.name = "attending_shool_now",
+         i.check.current_value = as.character(attending_shool_now),
+         i.check.value = "NA",
+         i.check.issue_id = "logic_c_attending_shool_now_not_grthan_enrolled_girls",
+         i.check.issue = glue("attending_shool_now_not_grthan_total_enrolled_children, attending_shool_now: {attending_shool_now} but int.total_enrolled_children: {int.total_enrolled_children}"),
+         i.check.other_text = "",
+         i.check.checked_by = "",
+         i.check.checked_date = as_date(today()),
+         i.check.comment = "", 
+         i.check.reviewed = "",
+         i.check.adjust_log = "",
+         i.check.so_sm_choices = "") |> 
+  filter(!is.na(i.check.current_value)) |> 
+  supporteR::batch_select_rename(input_selection_str = "i.check.", input_replacement_str = "")
+
+add_checks_data_to_list(input_list_name = "checks_output", input_df_name = "df_logic_c_attending_shool_now_not_grthan_total_enrolled_children")
 
 # combined  checks ------------------------------------------------------------
 
